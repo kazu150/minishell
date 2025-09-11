c043a5d0f93a7cf57726afc8ad9d2fd10c9f8fc9
Kazushi Isogai <kaisogai@c4r1s11.42tokyo.jp>
2025-09-11 15:38:21 +0900

    a

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// gcc main.c - lreadline - o main
+int	main(void)
+{
+	char *line = NULL;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (line == NULL || strlen(line) == 0)
+		{
+			free(line);
+			break ;
+		}
+		printf("line is %s\n", line);
+		// add_history(line);
+		free(line);
+	}
+	printf("exit\n");
+	return (0);
+}
