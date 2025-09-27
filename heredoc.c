5ac2edb70950e3dc512433d9ae6f30c065f49c30
Kazushi Isogai <kaisogai@c5r1s2.42tokyo.jp>
2025-09-27 18:19:25 +0900

    a

--- heredoc.c
+++ heredoc.c
@@ -6,7 +6,7 @@
 /*   By: kaisogai <kaisogai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 14:55:23 by kaisogai          #+#    #+#             */
-/*   Updated: 2025/09/27 17:35:00 by kaisogai         ###   ########.fr       */
+/*   Updated: 2025/09/27 18:19:20 by kaisogai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,18 +42,14 @@ int	setup_heredoc(char *target)
 	{
 		line = readline("heredoc> ");
 		if (line == NULL)
-		{ // Ctrl-D / EOF
-			// bash 互換にするなら警告を出してもよい
 			break ;
-		}
-		if (ft_strncmp(target, line, ft_strlen(target)) == 0)
-		{ // 完全一致で終了
+		if (ft_strncmp(target, line, ft_strlen(target) + 1) == 0)
+		{
 			free(line);
 			break ;
 		}
-		// 行 → 書き端（改行を付与）
-		if (write_all(fds[1], line, strlen(line)) < 0 || write_all(fds[1], "\n",
-				1) < 0)
+		if (write_all(fds[1], line, ft_strlen(line)) < 0 || write_all(fds[1],
+				"\n", 1) < 0)
 		{
 			free(line);
 			close(fds[0]);
